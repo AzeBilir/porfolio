c5c281472d9f7ee7581bdcd725ed61f9f5d8466d
Aze Bilir <azebiliraze@gmail.com>
2022-03-30 01:19:04 +0300

    adding homepage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.scss'
 import { Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
 import Layout from './components/Layout'
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />}></Route>
+        <Route path="/" element={<Layout />} />
+        <Route index element={<Home />} />
       </Routes>
     </>
   )
